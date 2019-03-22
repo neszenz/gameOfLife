296c6aa64ee2bd8be916c60631f2d53c6c347893
Tobias Newe <tobias.newe@t-online.de>
2019-03-22 15:59:18 +0000

    introduced game board class for random r/w cell access

---
+++ include/gol_board.hpp
@@ -0,0 +1,14 @@
+#ifndef GOL_BOARD_HPP
+#define GOL_BOARD_HPP
+
+namespace gol {
+
+    class Board {
+        public:
+            Board();
+            ~Board();
+    };
+
+}
+
+#endif//GOL_BOARD_HPP
---
+++ src/gol_board.cpp
@@ -0,0 +1,9 @@
+#include "gol_board.hpp"
+
+using namespace gol;
+
+Board::Board() {
+}
+
+Board::~Board() {
+}
