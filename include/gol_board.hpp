c4a71217b5fc3241ee8ad75467ccf0cfddaabf61
Tobias Newe <tobias.newe@t-online.de>
2019-03-25 21:40:55 +0000

    first straight forward and untested game board implementation

--- include/gol_board.hpp
+++ include/gol_board.hpp
@@ -1,5 +1,14 @@
+/** game of life - game board
+ * This game board keeps track of all Cell data, where they are and whether they
+ * are alive or not.
+ * Currently it is implemented for 2-dimensional boards only and not optimised
+ * for performance, but the general abstraction is meant to easily improve those
+ * aspects.
+ */
 #ifndef GOL_BOARD_HPP
 #define GOL_BOARD_HPP
+
+#include <vector>
 
 namespace gol {
 
@@ -7,6 +16,26 @@ namespace gol {
         public:
             Board();
             ~Board();
+
+            bool isAlive(int x, int y);
+            bool isDead(int x, int y);
+
+            void setAlive(int x, int y);
+            void setDead(int x, int y);
+
+        private:
+            struct Cell {
+                    int x;
+                    int y;
+                    bool isAlive = true;
+            };
+
+            std::vector<Cell> m_cells;
+
+            Cell* findCell(int x, int y);
+            void insertCell(int x, int y, bool isAlive);
+
+            void setCellStatus(int x, int y, int isAlive);
     };
 
 }
--- src/gol_board.cpp
+++ src/gol_board.cpp
@@ -7,3 +7,44 @@ Board::Board() {
 
 Board::~Board() {
 }
+
+bool Board::isAlive(int x, int y) {
+    Cell* cell = this->findCell(x, y);
+    if (cell != nullptr) {
+        return cell->isAlive;
+    }
+
+    return false;
+}
+bool Board::isDead(int x, int y) {
+    return !(this->isAlive(x, y));
+}
+
+void Board::setAlive(int x, int y) {
+    this->setCellStatus(x, y, true);
+}
+void Board::setDead(int x, int y) {
+    this->setCellStatus(x, y, false);
+}
+
+Board::Cell* Board::findCell(int x, int y) {
+    for (Board::Cell& cell : m_cells) {
+        if (cell.x == x && cell.y == y) {
+            return &cell;
+        }
+    }
+
+    return nullptr;
+}
+void Board::insertCell(int x, int y, bool isAlive) {
+    m_cells.emplace_back(Board::Cell({.x = x, .y = y, .isAlive = isAlive}));
+}
+
+void Board::setCellStatus(int x, int y, int isAlive) {
+    Board::Cell* existing_cell = this->findCell(x, y);
+    if (existing_cell != nullptr) {
+        existing_cell->isAlive = isAlive;
+    } else {
+        this->insertCell(x, y, isAlive);
+    }
+}
